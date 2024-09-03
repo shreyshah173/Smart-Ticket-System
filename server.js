7a9760a71f7cd108fa8f173c0ca431d8a0bae7f8
shreyshah173 <122092519+shreyshah173@users.noreply.github.com>
2024-09-03 18:22:56 +0530

    Fixing render

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Yoo babes');
 });
 
 app.listen(4000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 4000');
 });
